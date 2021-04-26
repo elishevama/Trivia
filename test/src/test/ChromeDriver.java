64e56a616ad704ef1f7080cbdb0abf12639c9835
elisheva <elisheva1703@gmail.com>
2021-04-26 22:30:17 +0300

    first github project

---
+++ test/src/test/ChromeDriver.java
@@ -0,0 +1,21 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+
+public class ChromeDriver {
+
+	
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			
+			
+	 ChromeDriver driver=new ChromeDriver();
+	 driver.get("https://www.google.co.il");
+	}
+
+		private void get(String string) {
+			// TODO Auto-generated method stub
+			
+		}
+
+}
